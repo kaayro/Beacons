90236ebf61432bddf920ce4109c044610b24784b
Carlos Celma <solrac@igitsoft.com>
2016-05-09 14:13:02 -0500

    advertising

--- actions.js
+++ actions.js
@@ -3,7 +3,8 @@ var fn = {
 		document.addEventListener("deviceready", fn.init, false);
 	},
 	init: function(){
-		fn.startMonitoringASingleiBeacon();
+		//fn.startMonitoringASingleiBeacon();
+		fn.advertising();
 	},
 	startMonitoringASingleiBeacon: function(){
 		var logToDom = function (message) {
@@ -97,6 +98,44 @@ var fn = {
         cordova.plugins.locationManager.startRangingBeaconsInRegion(beaconRegion)
             .fail(console.error)
             .done();
+	},
+	advertising: function(){
+		var uuid = '00000000-0000-0000-0000-000000000000';
+		var identifier = 'advertisedBeacon';
+		var minor = 2000;
+		var major = 5;
+		var beaconRegion = new cordova.plugins.locationManager.BeaconRegion(identifier, uuid, major, minor);
+
+		// The Delegate is optional
+		var delegate = new cordova.plugins.locationManager.Delegate();
+
+		// Event when advertising starts (there may be a short delay after the request)
+		// The property 'region' provides details of the broadcasting Beacon
+		delegate.peripheralManagerDidStartAdvertising = function(pluginResult) {
+			console.log('peripheralManagerDidStartAdvertising: '+ JSON.stringify(pluginResult.region));
+		};
+		// Event when bluetooth transmission state changes 
+		// If 'state' is not set to BluetoothManagerStatePoweredOn when advertising cannot start
+		delegate.peripheralManagerDidUpdateState = function(pluginResult) {
+			console.log('peripheralManagerDidUpdateState: '+ pluginResult.state);
+		};
+
+		cordova.plugins.locationManager.setDelegate(delegate);
+
+		// Verify the platform supports transmitting as a beacon
+		cordova.plugins.locationManager.isAdvertisingAvailable()
+			.then(function(isSupported){
+
+				if (isSupported) {
+					cordova.plugins.locationManager.startAdvertising(beaconRegion)
+						.fail(conole.error)
+						.done();
+				} else {
+					console.log("Advertising not supported");
+				}
+			})
+			.fail(function(e) { console.error(e); })
+			.done();
 	}
 };
 window.addEventListener("load",fn.ready,false);
